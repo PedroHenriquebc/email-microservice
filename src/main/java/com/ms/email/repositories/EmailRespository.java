f88b18ac295cd4bc114530b0d55fbc14d09ac705
Pedro Henrique <pedrohbc96@gmail.com>
2023-02-20 09:53:22 -0300

    Altera tipo do ID de Email

--- src/main/java/com/ms/email/repositories/EmailRespository.java
+++ src/main/java/com/ms/email/repositories/EmailRespository.java
@@ -4,6 +4,8 @@ import com.ms.email.models.EmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface EmailRespository extends JpaRepository<EmailModel, Long> {
+public interface EmailRespository extends JpaRepository<EmailModel, UUID> {
 }
